fc21b8d7097527c16fbe3d2cd2bd8a506a32e338
Ashmin Jayson <ashminjayson10@gmail.com>
2023-03-22 11:45:27 +0530

    Add files via upload

--- Experiment_4_Largest_Row_Sum/program.r
+++ Experiment_4_Largest_Row_Sum/program.r
@@ -12,11 +12,28 @@ cat('The input dataframe is :')
 print(df)
 
 df = sapply(df, as.numeric)
-df2 <- df[!duplicated(df), ]
-cat('The dataframe without duplicate rows is :')
-print(df2)
 
-rs = rowSums(df2)
+x = nrow(df)
+i = 0
+while (i <= x) {
+  j = i + 1
+  while(j <= x) {
+    if (setequal(df[j, ], df[i, ])) {
+      df = df[-j, ]
+      x = x - 1
+    }
+    else {
+    j = j + 1
+    }
+  }
+  i = i + 1
+}
+
+cat('The dataframe without duplicate rows is :')
+print(df)
+
+rs = rowSums(df)
 maxrs = max(rs)
 cat('The max row sum is : ', maxrs)
 cat('\nThe factorial of max row sum is : ', fact(maxrs))
+
